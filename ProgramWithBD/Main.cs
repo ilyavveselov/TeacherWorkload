34ae3f89cea4b7f051f238b7da78db47bb10a982
ilyavveselov <seaatsky.g@gmail.com>
2022-01-15 00:49:11 +0400

    тестю новую ветку

--- ProgramWithBD/Main.cs
+++ ProgramWithBD/Main.cs
@@ -225,6 +225,7 @@ namespace ProgramWithBD
 
         private void Main_Load(object sender, EventArgs e)
         {
+            //Console.WriteLine("Сделал для теста новой ветки в git (feature)");
             try
             {
                 using (var db = new WorkloadsDataContext())
